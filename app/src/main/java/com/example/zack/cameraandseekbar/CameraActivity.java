9c506316312d34991b8233f65320e429759e7af6
Zack Robinson <zwrprofessional@gmail.com>
2017-09-01 00:22:40 -0400

    Initial commit

---
+++ app/src/main/java/com/example/zack/cameraandseekbar/CameraActivity.java
@@ -0,0 +1,46 @@
+package com.example.zack.cameraandseekbar;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class CameraActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, Constants.REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Constants.REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView mImageView = (ImageView) findViewById(R.id.capturedImage);
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    public void goToEMI(View view) {
+        Intent intent = new Intent(this, SeekbarActivity.class);
+        startActivity(intent);
+    }
+
+
+
+}
